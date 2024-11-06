eda5772c993549ba62f88f4e817c3ee5fb94a354
xin <114647677+xinyihsu@users.noreply.github.com>
2024-11-06 22:20:14 +0800

    Add export file

--- src/components/login/other.vue
+++ src/components/login/other.vue
@@ -23,6 +23,64 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { Label } from "@/components/ui/label";
 import { File } from "lucide-vue-next";
+import { ref, watchEffect } from "vue";
+
+const toggleValue = ref<string[]>([]);
+const selectAll = ref<boolean>(false);
+
+const handleExport = () => {
+  chrome.storage.local.get("users", function(result) {
+    let selectedData = {};
+  
+    toggleValue.value.forEach((item) => {
+      if (result.users?.[item]) {
+        selectedData = {
+          ...selectedData,
+          [item]: result.users?.[item]
+        };
+      }
+    });
+
+    console.log(selectedData);
+
+    if (selectedData) {
+      // 轉換 users 資料為 JSON 字符串
+      const jsonData = JSON.stringify(selectedData, null, 2);
+
+      // 創建一個 Blob 文件
+      const blob = new Blob([jsonData], { type: 'application/json' });
+
+      // 創建一個 URL 來下載該 Blob 文件
+      const url = URL.createObjectURL(blob);
+
+      // 使用 chrome.downloads.download 下載文件
+      chrome.downloads.download({
+        url: url,
+        filename: 'users.json',
+        saveAs: true
+      });
+    } else {
+      console.log('No users data found in chrome.storage.local');
+    }
+  });
+}
+
+watchEffect(() => {
+  // if (toggleValue.value.length != 5) {
+  //   toggleValue.value = toggleValue.value.filter((item) => item !== 'all');
+  // }
+  if (!selectAll.value && toggleValue.value.includes('all')) {
+    toggleValue.value = ['dev', 'uat', 'staging', 'prod', 'all'];
+    selectAll.value = true;
+  }
+  else if (selectAll.value && toggleValue.value.length != 5) {
+    toggleValue.value = toggleValue.value.filter((item) => item !== 'all');
+    selectAll.value = false;
+  }
+
+  console.log(toggleValue.value);
+});
+
 </script>
 
 <template>
@@ -103,43 +161,44 @@ import { File } from "lucide-vue-next";
               </CardHeader>
               <CardContent class="space-y-2">
                 <ToggleGroup
+                  v-model="toggleValue"
                   type="multiple"
                   class="flex flex-col content-start items-start"
                 >
                   <ToggleGroupItem value="dev">
                     <div class="items-top flex space-x-2">
-                      <Checkbox id="dev" />
+                      <Checkbox id="dev" :checked="toggleValue.includes('dev')" />
                       <Label for="dev"> DEV </Label>
                     </div>
                   </ToggleGroupItem>
                   <ToggleGroupItem value="uat">
                     <div class="items-top flex space-x-2">
-                      <Checkbox id="uat" />
+                      <Checkbox id="uat" :checked="toggleValue.includes('uat')" />
                       <Label for="uat"> UAT </Label>
                     </div>
                   </ToggleGroupItem>
                   <ToggleGroupItem value="staging">
                     <div class="items-top flex space-x-2">
-                      <Checkbox id="staging" />
+                      <Checkbox id="staging" :checked="toggleValue.includes('staging')" />
                       <Label for="staging"> STAGING </Label>
                     </div>
                   </ToggleGroupItem>
                   <ToggleGroupItem value="prod">
                     <div class="items-top flex space-x-2">
-                      <Checkbox id="prod" />
+                      <Checkbox id="prod" :checked="toggleValue.includes('prod')" />
                       <Label for="prod"> PROD </Label>
                     </div>
                   </ToggleGroupItem>
                   <ToggleGroupItem value="all">
                     <div class="items-top flex space-x-2">
-                      <Checkbox id="all" />
+                      <Checkbox id="all" :checked="toggleValue.includes('all')"/>
                       <Label for="all"> ALL </Label>
                     </div>
                   </ToggleGroupItem>
                 </ToggleGroup>
               </CardContent>
               <CardFooter>
-                <Button class="w-full">Export</Button>
+                <Button class="w-full" @click="handleExport">Export</Button>
               </CardFooter>
             </Card>
           </TabsContent>
